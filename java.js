2b640698dd71e14f3b16140c85d32296a53e6e9c
Akshay Gaddi <76643881+organicboi@users.noreply.github.com>
2022-12-15 22:21:09 +0530

    add noraml files

---
+++ java.js
@@ -0,0 +1,19 @@
+// alert("hekki");
+$('.referenceLink').click(function () {
+  location.href = './refrenceLink/NEw.html'
+})
+$('.timetable').click(function () {
+  location.href = './timetable/index.html'
+})
+$('.examform').click(function () {
+  location.href = './examform/index.html'
+})
+$('.announcement').click(function () {
+  location.href = './Announcements/index.html'
+})
+$('.attendance').click(function () {
+  location.href = './refrenceLink/NEw.html'
+})
+$('.community').click(function () {
+  location.href = './community/index.html'
+})
